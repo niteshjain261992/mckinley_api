363a6d24a4debdc6732f0172be33b898b51b5fee
nitesh <niteshjain261992@gmail.com>
2019-03-13 23:49:51 +0530

    handle routes error

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,3 +1,10 @@
 module.exports = (app)=> {
     require("../routes/user")(app);
+
+    app.use((err, req, res, callback)=> {
+        const code = err.status || 500;
+        res.status(code);
+        res.json({ code: code, status: 'ERR', error: { error: err.message }})
+    });
+
 };
